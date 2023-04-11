f2d36c17e64e7b2fc11ef4888335b001abff1db7
Elga <105276166+Eliegautie@users.noreply.github.com>
2023-04-11 11:14:02 +0200

    Update and rename bollinger-bands.py to Bollinger bands.py

--- Bollinger bands.py
+++ Bollinger bands.py
@@ -1,13 +1,3 @@
-# Bollinger Bands - short introduction
-
-Bollinger Bands consist of :
-- an N-period moving average (MA), 
-- an upper band at K times an N-period standard deviation above the moving average (MA + Kσ), 
-- and a lower band at K times an N-period standard deviation below the moving average (MA − Kσ).
-
-Typical values for N and K are **20** days and **2**, respectively.
-
-
 import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd
@@ -17,11 +7,13 @@ import yfinance as yf
 
 df = yf.download(tickers='BNP.PA', period = '1d', interval ='1m')
 
-#Interval required 1 minute
+# Interval required 1 minute
 
 df['Middle Band'] = df['Close'].rolling(window=20).mean() 
 df['Lower Band'] = df['Middle Band'] - 2*df['Close'].rolling(window=20).std()
 df['Upper Band'] = df['Middle Band'] + 2*df['Close'].rolling(window=20).std()
+
+
 
 # declare figure
 
@@ -32,6 +24,7 @@ fig.add_trace(go.Scatter(x=df.index, y = df['Upper Band'], line=dict(color='red'
 fig.add_trace(go.Scatter(x=df.index, y = df['Lower Band'], line=dict(color='green', width=1.5),name = 'Lower Band'))
 
 # Candlestick
+
 fig.add_trace(go.Candlestick(x=df.index,
                              open = df['Open'],
                              high = df['High'],
@@ -39,12 +32,14 @@ fig.add_trace(go.Candlestick(x=df.index,
                              close = df['Close'], name = 'Market Data'))
 
 # Add titles
+
 fig.update_layout(
     title = 'BNP live share price evolution',
     yaxis_title = 'Stock Price (EUR per Share)')
 
 
 # Add range slider
+
 fig.update_layout(
     xaxis=dict(
         rangeselector=dict(
@@ -76,6 +71,5 @@ fig.update_layout(
 )
 
 # Show Figure
+
 fig.show()
-
-
