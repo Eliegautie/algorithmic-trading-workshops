3f600bc4c0de2076d9ee4e681330a457ed3aabcd
Elga <105276166+Eliegautie@users.noreply.github.com>
2023-05-04 20:32:09 +0200

    Update RSI.py

--- RSI.py
+++ RSI.py
@@ -8,13 +8,12 @@ import talib
 
 
 class Trading(Link):
-	def __init__(self):		
-        super().__init__()
-        self.closes = []
+	def __init__(self):
+		super().__init__()
 		self.src = 'bitmex' # exchange name
-        self.venue = 'BitMEX' # API key name
-        self.sym = 'XBTUSD'  # symbol we will trade
-        self.level = '1m' # OHLC candle granularity
+		self.sym = 'XBTUSD'  # symbol we will trade
+		self.level = '1m' # OHLC candle granularity	
+      
 		
 		
 	def on_startup(self):
@@ -31,16 +30,15 @@ class Trading(Link):
 			self.best_ask = data['ask'][0]	
 	
 		 
-    def trade_update(self, src, sym, data):		
-        """Event: receive market trades from subscribed symbols"""
-		candles = self.fetch_candles(self.venue, sym=self.sym, level=self.level)['data']
+    def trade_update(self, src, sym, data):        
+	candles = self.fetch_candles(self.venue, sym=self.sym, level=self.level)['data']
         self.closes = [x['close'] for x in candles]
-		closes = array(self.closes)
+	closes = array(self.closes)
 		
-		RSI = talib.RSI(closes)	
+	RSI = talib.RSI(closes)	
 			
-		if RSI[-1] > 75:      
-			self.create_limit_order(self.venue, sym=self.sym, side="Sell", size=100, price=self.best_ask)
+	if RSI[-1] > 75:      
+		self.create_limit_order(self.venue, sym=self.sym, side="Sell", size=100, price=self.best_ask)
 			
-		if RSI[-1] < 25:
-			self.create_limit_order(self.venue, sym=self.sym, side="Buy", size=100, price=self.best_bid)
+	if RSI[-1] < 25:
+		self.create_limit_order(self.venue, sym=self.sym, side="Buy", size=100, price=self.best_bid)
