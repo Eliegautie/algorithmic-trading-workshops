954a7ed4d1a0bbfb50c502e342ad17186ac475b8
Elga <105276166+Eliegautie@users.noreply.github.com>
2023-05-23 17:51:17 +0200

    Update RSI.py
    For now, I just get the live RSI indicator and log out the value (logger.info((self.rsi))

--- script/RSI.py
+++ script/RSI.py
@@ -22,24 +22,21 @@ class Trading(Link):
     
     def __init__(self):
         super().__init__()
-		
         # ALGO PARAMS
-        self.src = 'bitmex'                         # exchange name
-        self.venue = 'BitMEX'                         # API key name
+        self.src = 'bitmex'                          # exchange name
+        self.venue = 'BitMEX'                        # API key name
         self.sym = 'XBTUSD'                          # symbol we will trade
-        self.level = '1m'                             # OHLC candle granularity
-        self.lookback = 150                         # lookback period of close prices 
+        self.level = '1m'                            # OHLC candle granularity
+        self.lookback = 150                          # lookback period of close prices 
         self.time_step = TIME_LOOKUP[self.level]     # time step in milliseconds
         
         # ALGO STRATEGY STATE
-        self.closes = dict()                         # time bin -> close price
-        self.rsi = dict(rsi=np.nan)                  # MACD histogram and slope value                    
-        self.risk = 0                                # current position risk for sym
-        self.orders = dict(bid={}, ask={})           # current open limit orders
+        self.closes = dict()                          # time bin -> close price
+        self.rsi = dict(rsi=np.nan)                   # MACD histogram and slope value
+        self.risk = 0                                 # current position risk for sym
+        self.orders = dict(bid={}, ask={})            # current open limit orders
         
         # ALGO PARAMS        
-        self.fee_cost = 4e-4                        # cost of entering and exiting position
-	self.max_risk = 200                         # max position risk limit
         
         # RUN ON STARTUP
         self.on_startup()
@@ -51,7 +48,8 @@ class Trading(Link):
     # MARKET DATA STATE     
     def on_startup(self):
         self.fetch_latest_closes()
-        self.fetch_current_risk()        
+        self.fetch_current_risk()
+        
         
 
     def fetch_latest_closes(self):
@@ -69,8 +67,9 @@ class Trading(Link):
             if x['sym'] == self.sym:
                 sign = 1 if x['side'] == 'Buy' else -1
                 self.risk = sign * x['pos_size']
-    
-       
+            
+      
+     
     @property
     def last_closes(self):
         start_time = self.time_bin_now - self.lookback * self.time_step
@@ -81,113 +80,17 @@ class Trading(Link):
     def update_signal(self):
         closes = self.last_closes 
         if not any(np.isnan(closes)):        
-            rsi = talib.RSI(self.last_closes)
-			self.rsi['rsi'] = rsi[-1] 
+            rsi = talib.RSI(self.last_closes) 
+			self.rsi['rsi'] = rsi[-1]
 			logger.info((self.rsi))
+          
             
     def update_close(self, data):
         time_bin = self.candle_bin(data['time'], self.level)
-        self.closes[time_bin] = data['price']    
+        self.closes[time_bin] = data['price']   
 		
-	@property
-    def orders_intent(self):  
-		bid = self.best_bid
-		ask = self.best_ask
-        bsize = np.clip(self.max_risk - self.risk, 0, self.max_risk)
-        asize = np.clip(self.max_risk + self.risk, 0, self.max_risk)
-        bsize = self.round_value(bsize, 100)
-        asize = self.round_value(asize, 100)
-        return {'bid': (bid, bsize), 'ask': (ask, asize)}
-	
-	def update_limit_orders(self):
-        intent = self.orders_intent
-        bid, bsize = intent['bid']
-        ask, asize = intent['ask']
-        
-        log_msg = {
-            'rsi': np.round(self.rsi['rsi'], 4),            
-            'bid': [bid, bsize],
-            'ask': [ask, asize]
-        }
-        
-        if not any(np.isnan((bid, ask, asize, bsize))):
-            cancels, updates, inserts = [], [], []
-            
-            book_side = ['bid', 'ask']
-            
-            for key in book_side:
-                sort_key = lambda x: (1 if key == 'ask' else -1) * x['order_price']
-                orders = sorted(self.orders[key].values(), key=sort_key)
-                
-                price, size = intent[key]
-                side = 'Sell' if key == 'ask' else 'Buy'
-                
-                if len(orders) > 0:
-                    update = dict()
-                    top, *remain = orders
-                    
-                    if price != top['order_price']: update['price'] = price
-                    if size != top['remain_size']: update['size'] = size
-                    
-                    if len(update) > 0:
-                        update['order_id'] = top['order_id']
-                        updates.append(update)
-                    
-                    cancels.extend([x['order_id'] for x in remain])
-                
-                elif size > 0:
-                    inserts.append({'sym': self.sym, 'side': side, 'size': size, 'price': price})
-                    
-            for order_id in cancels:
-                for x in self.cancel_order(self.venue, order_id=order_id)['data']:
-                    key = 'bid' if x['side'] == 'Buy' else 'ask'
-                    self.orders[key].pop(x['order_id'], None)
-                
-            for update in updates:
-                data = self.amend_order(self.venue, **update)['data']
-                key = 'bid' if data['side'] == 'Buy' else 'ask'
-                self.orders[key][data['order_id']] = data
-                
-            for insert in inserts:
-                data = self.create_limit_order(self.venue, **insert)['data']
-                key = 'bid' if data['side'] == 'Buy' else 'ask'
-                self.orders[key][data['order_id']] = data
-                
-        logger.info('\n' + json.dumps(log_msg))
-        
-                            
-            
-    # TRADING EVENTS
-    def order_update(self, src, sym, data):
-        if sym == self.sym:
-            if data.get('remain_size') == 0:
-                key = 'bid' if data['side'] == 'Buy' else 'ask'
-                self.orders[key].pop(data['order_id'], None)
-                
-    def fill_update(self, src, sym, data):
-        if sym == self.sym:
-            sign = 1 if data['side'] == 'Buy' else -1
-            self.risk = round(self.risk + sign * data['fill_size'])
-            self.update_limit_orders() # update limit orders on risk change
-        
-    def quote_update(self, src, sym, data):
-        if sym == self.sym:
-			self.best_bid = data['bid'][0]
-			self.best_ask = data['ask'][0]	
-
-    def trade_update(self, src, sym, data):
+	def trade_update(self, src, sym, data):
         if sym == self.sym:
             self.update_close(data)
             self.update_signal()
-
-    # WEBHOOKS
-    @http.route
-    def get_state(self, data):
-        return {k: getattr(self, k, None) for k in data.get('keys', [])}
-    
-    @http.route    
-    def post_state(self, data):
-        for k, v in data.items():
-            if hasattr(self, k):
-                setattr(self, k, v)
-        return self.get_state({'keys': list(data)})    
+ 	
